1075bd258b1f93d19167b9229d68d94aaffa04e4
Maxim Lepekha <maxim@lepekha.com>
2026-06-29 01:21:05 +0200

    chore: scaffold Nuxt 4 + Nuxt UI + Vitest

---
+++ app/pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>Voice PE LED</h1>
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-01-01',
+  modules: ['@nuxt/ui'],
+  ssr: true,
+  app: {
+    head: {
+      title: 'Voice PE LED',
+      meta: [{ name: 'viewport', content: 'width=device-width, initial-scale=1' }],
+    },
+  },
+})
