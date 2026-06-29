fac30b304f23c30634f3cff9ad4cf4dac4896f58
Maxim Lepekha <maxim@lepekha.com>
2026-06-29 02:07:28 +0200

    feat: add LED-ring favicon

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,10 @@
+<script setup lang="ts">
+const { app } = useRuntimeConfig()
+useHead({
+  link: [{ rel: 'icon', type: 'image/svg+xml', href: `${app.baseURL}favicon.svg` }],
+})
+</script>
+
 <template>
   <UApp>
     <NuxtPage />
